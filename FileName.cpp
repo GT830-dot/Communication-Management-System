4a70a9cc817c0b8573f2023510e620750de13849
GT830-dot <zqx2530nlp@outlook.com>
2026-01-14 21:11:48 +0800

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void num(int* p, int b);
+int main() {
+	int arr[9] = { 4,5,6,1,2,3,8,9,0 };
+	cout << "ÅÅĞòÇ°£º" << endl;
+	for (int a = 0; a < 9; a++) {
+		cout << arr[a] << " ";
+	}
+	cout << "ÅÅĞòºó£º" << endl;
+	int c = sizeof(arr) / sizeof(arr[0]);
+	num(arr, c);
+	for (int e = 0; e < 9; e++) {
+		cout << arr[e] << " ";
+	}
+    return 0;
+}
+void num(int* p, int b) {
+	for (int i = 0; i < b - 1; i++) {
+		for (int j = 0; j < b - i - 1; j++) {
+			if (p[j] > p[j + 1]) {
+				int temp = p[j];
+				p[j] = p[j + 1];
+				p[j + 1] = temp;
+			}
+		}
+	}
+}
